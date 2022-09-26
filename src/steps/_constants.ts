c2c700bc5aa7b83953ea7269423edae3ae196c77
alecdwm <alec@owls.io>
2022-09-26 16:38:03 +0000

    fixed logo urls

--- src/steps/_constants.ts
+++ src/steps/_constants.ts
@@ -3,15 +3,15 @@ import { EvmNativeModule } from '@talismn/balances-evm-native'
 import { SubNativeModule } from '@talismn/balances-substrate-native'
 import { SubOrmlModule } from '@talismn/balances-substrate-orml'
 
-export const githubChaindataBranch = 'feat/split-entities' // 'main'
+export const githubChaindataBranch = 'v3'
 export const githubChaindataBaseUrl = `https://raw.githubusercontent.com/TalismanSociety/chaindata/${githubChaindataBranch}`
 export const githubChainsUrl = `${githubChaindataBaseUrl}/chaindata.json`
 export const githubTestnetChainsUrl = `${githubChaindataBaseUrl}/testnets-chaindata.json`
 export const githubEvmNetworksUrl = `${githubChaindataBaseUrl}/evm-networks.json`
 export const githubTokensUrl = `${githubChaindataBaseUrl}/tokens.json`
-export const githubChainLogoUrl = (chainId: string) => `githubChaindataBaseUrl/assets/${chainId}/logo.svg`
-export const githubEvmNetworkLogoUrl = (networkId: string) => `githubChaindataBaseUrl/assets/${networkId}/logo.svg`
-export const githubTokenLogoUrl = (tokenId: string) => `githubChaindataBaseUrl/token-assets/${tokenId}.svg`
+export const githubChainLogoUrl = (chainId: string) => `${githubChaindataBaseUrl}/assets/${chainId}/logo.svg`
+export const githubEvmNetworkLogoUrl = (networkId: string) => `${githubChaindataBaseUrl}/assets/${networkId}/logo.svg`
+export const githubTokenLogoUrl = (tokenId: string) => `${githubChaindataBaseUrl}/token-assets/${tokenId}.svg`
 
 export const balanceModules: any[] = [
   // ExampleModule,
